e4165328376db2cd413f42fe80621315b0056444
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-05-27 10:35:31 +0200

    test: Fix Capybara headless_chrome

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,20 @@ require "decidim/homepage_interactive_map/test/w3c_rspec_validators_overrides"
 require "./spec/support/custom_matchers"
 
 SimpleCov.start if ENV["SIMPLECOV"]
+
+Capybara.register_driver :headless_chrome do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.args << "--headless=new"
+  options.args << "--no-sandbox"
+  options.args << if ENV["BIG_SCREEN_SIZE"].present?
+                    "--window-size=1920,3000"
+                  else
+                    "--window-size=1920,1080"
+                  end
+  options.args << "--ignore-certificate-errors" if ENV["TEST_SSL"]
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    capabilities: [options]
+  )
+end
