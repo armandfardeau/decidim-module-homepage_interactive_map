138b1d4ecdc01b91d6c3ca6e97a7891404f83a51
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-07-05 10:31:06 +0200

    revert(ci): Install proj sh

--- .github/install_proj.sh
+++ .github/install_proj.sh
@@ -1,9 +1,7 @@
-sudo apt-get update && sudo apt-get install -y cmake sqlite libtiff-dev curl libcurl4-openssl-dev libssl-dev libproj-dev proj-bin
-
-#  curl https://download.osgeo.org/proj/"${PROJ_VERSION}".tar.gz -o "${PROJ_VERSION}".tar.gz
+sudo apt-get update && sudo apt install cmake sqlite libtiff-dev curl libcurl4-openssl-dev libssl-dev -y
 
 if [ ! -d "$PROJ_VERSION" ]; then
-  curl -L https://download.osgeo.org/proj/"${PROJ_VERSION}".tar.gz -o "${PROJ_VERSION}".tar.gz
+  curl https://download.osgeo.org/proj/"${PROJ_VERSION}".tar.gz -o "${PROJ_VERSION}".tar.gz
   tar -xzf "${PROJ_VERSION}".tar.gz
 fi
 
@@ -17,17 +15,3 @@ cd build || exit
 cmake ..
 sudo cmake --build . -j "$(nproc)" --target install
 sudo ldconfig
-
-export PROJ_DIR=/usr/local
-export PROJ_LIB=/usr/local/lib
-export PROJ_INCLUDE=/usr/local/include
-
-proj
-
-echo "PROJ_DIR: $PROJ_DIR"
-echo "PROJ_LIB: $PROJ_LIB"
-echo "PROJ_INCLUDE: $PROJ_INCLUDE"
-
-gem install rgeo-proj4 -- --with-proj-dir=$PROJ_DIR --with-proj-include=$PROJ_INCLUDE --with-proj-lib=$PROJ_LIB
-bundle config build.rgeo-proj4 --with-proj-dir=$PROJ_DIR --with-proj-include=$PROJ_INCLUDE --with-proj-lib=$PROJ_LIB
-bundle install
